81b8d00f9a0b42cf2da8308292757db47f353186
Damon Kelley <damon.kelley@gmail.com>
2023-01-02 13:41:08 -0500

    Add layout for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="mx-auto max-w-7xl p-4 sm:p-6 lg:p-8">
-      <main>
-        <div className="space-y-4 p-6">
-          <h1 className="text-2xl   font-rounded">Hi - I&apos;m Tylee</h1>
+    <div className="mx-auto max-w-screen-xl">
+      <main className="grid grid-cols-1 gap-8 p-8 md:grid-cols-2 lg:gap-16 lg:p-16">
+        <div className="mx-auto max-w-lg space-y-4">
+          <h1 className="font-rounded text-2xl">Hi - I&apos;m Tylee</h1>
           <h2 className="italic">She / Her / They</h2>
           <p>
             I&apos;m a designer living in Brooklyn. Currently working at a small
@@ -16,18 +16,20 @@ export default function About() {
             learning better practices in design.
           </p>
           <p>
-            I&apos;m interested in learning more about accessibility in design and
-            how we can create new and interesting patterns for communication in
-            the digital age.
+            I&apos;m interested in learning more about accessibility in design
+            and how we can create new and interesting patterns for communication
+            in the digital age.
           </p>
         </div>
-        <Image
-          className="mt-6 rounded-lg"
-          src="/images/tylee.png"
-          alt="Profile of Tylee"
-          width={500}
-          height={500}
-        />
+        <div className="mx-auto max-w-lg">
+          <Image
+            className="rounded-lg"
+            src="/images/tylee.png"
+            alt="Profile of Tylee"
+            width={500}
+            height={500}
+          />
+        </div>
       </main>
     </div>
   );
