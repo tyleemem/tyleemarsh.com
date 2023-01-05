a19ab8a71f09cc605e2b42215fade30f535a06c1
Damon Kelley <damon.kelley@gmail.com>
2023-01-04 21:30:52 -0500

    Add svg for each project

--- pages/work.tsx
+++ pages/work.tsx
@@ -15,8 +15,24 @@ export default function Work() {
 
 function Project({ name }: { name: string }) {
   return (
-    <div className="flex h-72 w-72 items-center justify-center">
-      <div className="text-center font-rounded text-2xl">{name}</div>
+    <div className="grid h-72 w-72 grid-cols-1 items-center">
+      <div className="z-10 col-span-full row-span-full text-center font-rounded text-2xl">
+        {name}
+      </div>
+      <div className="z-0 col-span-full row-span-full">
+        <Shape />
+      </div>
     </div>
   );
 }
+
+const Shape = () => {
+  return (
+    <svg viewBox="0 0 420 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M0 10C0 4.47716 4.47715 0 10 0H410C415.523 0 420 4.47715 420 10V410C420 415.523 415.523 420 410 420H10C4.47716 420 0 415.523 0 410V10Z"
+        fill="#D75844"
+      />
+    </svg>
+  );
+};
