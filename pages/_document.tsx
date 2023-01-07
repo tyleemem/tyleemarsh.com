c09da1d010239dce6246e26a29c16e11abc25732
Damon Kelley <damon.kelley@gmail.com>
2023-01-07 16:15:35 -0500

    Add percy configuration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="mx-auto max-w-screen-2xl bg-cream font-sans">
+      <body >
         <Main />
         <NextScript />
       </body>
